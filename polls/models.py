0d7c7c474ec3460d58967912047b62cf68e6aabf
Joshua inyang <joshuainyang255@gmail.com>
2020-10-12 22:51:37 +0100

    another commit

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.db.models.signals import post_save
 
-# Create your models here.
+# class Contact(models.Model):
+#     """Portfolio model"""
+    
+#     email = models.EmailField(help_text='Example: joshuainyang255@gmail.com')
+#     subject = models.CharField(max_length=100, help_text='Subject of discussion')
+#     message = models.TextField(max_length=10000)
+    
+#     def __str__(self):
+#         return 'Contact Me!'
+
+#     def create_user_profile(sender, instance, created, **kwargs):
+#         """Creates a profile for each registered regular user"""
+
+#         if created and instance:
+#             Contact.objects.create(user=instance)
+
+#     post_save.connect(create_user_profile, sender=User)
+
+#     def save(self, *args, **kwargs):
+#         """Save profile and resize profile image"""
+
+#         super(Contact, self).save(*args, **kwargs)
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.views.generic import TemplateView, CreateView
+
 
 # Create your views here.
 def home(request):
     return render(request, 'polls/base.html')
+# class Home(TemplateView):
+#     template_name = 'polls/base.html'
+
+# class Contact(CreateView):
+#     """Contact Page"""
+
+#     model = Contact
+#     form_class = HireForm
+#     template_name = 'base/contact.html'
+
+#     def form_valid(self, form):
+#         contact = form.save()
+#         contact.save()
+#         messages.success(self.request, f'Your order has been sent! You would receive a reply shortly, thank you.')
+#         return redirect('contact') 
